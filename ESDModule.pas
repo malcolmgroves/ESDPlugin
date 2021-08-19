2e7bacc1cbc5464791cde637995104336a4428e8
Knut-Knoblauch <88841540+Knut-Knoblauch@users.noreply.github.com>
2021-08-19 09:37:42 -0500

    Add files via upload

--- ESDModule.pas
+++ ESDModule.pas
@@ -89,7 +89,7 @@ end;
 
 procedure	TESDPlugin.KeyDownForAction(const Action, Context: String; const JSONPayload: TJSONObject; const DeviceID: String);
 begin
-    GConnectionManager.SetImage(GetBase64Image('C:\Users\andyb\AppData\Roaming\Elgato\StreamDeck\IconPacks\de.streamdeck-fx.sdfxmatrix.sdIconPack\icons\elgato_fx_matrix_025_30C900.png'), Context, kESDSDKTarget_HardwareAndSoftware);
+    //GConnectionManager.SetImage(GetBase64Image('C:\Users\andyb\AppData\Roaming\Elgato\StreamDeck\IconPacks\de.streamdeck-fx.sdfxmatrix.sdIconPack\icons\elgato_fx_matrix_025_30C900.png'), Context, kESDSDKTarget_HardwareAndSoftware);
 end;
 
 //*************************************************************//
